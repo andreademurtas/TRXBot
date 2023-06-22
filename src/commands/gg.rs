3fb941eaae0cf38e938c55608792ec5e031ba6aa
Andrea De Murtas <56048157+andreademurtas@users.noreply.github.com>
2023-06-22 13:58:47 +0200

    gg

--- src/commands/gg.rs
+++ src/commands/gg.rs
@@ -8,6 +8,9 @@ async fn gg(ctx: &Context, msg: &Message) -> CommandResult {
     let channel_id = msg.channel_id;
     let red_flag = "🚩";
     let name = channel_id.name(&ctx).await.unwrap();
+    if name.contains(red_flag) {
+        return Ok(());
+    }
     channel_id.edit(&ctx.http, |c| {
         c.name(format!("{}{}", red_flag, name))
     }).await?;
--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -12,7 +12,7 @@ use serenity::utils::Colour;
 use crate::ShardManagerContainer;
 
 #[command]
-#[owners_only]
+#[allowed_roles("ring0")]
 async fn quit(ctx: &Context, msg: &Message) -> CommandResult {
     let data = ctx.data.read().await;
 
@@ -28,57 +28,54 @@ async fn quit(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
-#[owners_only]
+#[allowed_roles("ring0")]
 async fn new(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let data = ctx.data.read().await;
-    if let Some(_manager) = data.get::<ShardManagerContainer>() {
-        if let Some(ctf) = args.single::<String>().ok() {
-            if let Some(guild_id) = msg.guild_id {
-                let category = guild_id.create_channel(&ctx.http, |c| c.name(&ctf).kind(ChannelType::Category)).await?;
-                let general_public = guild_id.create_channel(&ctx.http, |c| c.name("general-public").kind(ChannelType::Text).category(category.id)).await?;
-                let guild = guild_id.to_partial_guild(&ctx.http).await?;
-                let rgb = Colour::from_rgb(rand::random::<u8>(), rand::random::<u8>(), rand::random::<u8>());
-                let role = guild.create_role(&ctx.http, |r| r.name(&ctf).colour(rgb.0 as u64)).await?;
-                let role_permission = PermissionOverwrite {
-                    allow: Permissions::all(),
-                    deny: Permissions::empty(),
-                    kind: PermissionOverwriteType::Role(role.id),
-                };
-                let everyone_permission = PermissionOverwrite {
-                    allow: Permissions::empty(),
-                    deny: Permissions::all(),
-                    kind: PermissionOverwriteType::Role(RoleId::from(guild.id.0)),
-                };
-                let general = guild_id.create_channel(&ctx.http, |c| c.name("general").kind(ChannelType::Text).category(category.id)).await?;
-                general.create_permission(&ctx.http, &everyone_permission).await?;
-                general.create_permission(&ctx.http, &role_permission).await?;
-                let category_permissions = PermissionOverwrite {
-                    allow: Permissions::all(),
-                    deny: Permissions::empty(),
-                    kind: PermissionOverwriteType::Role(role.id),
-                };
-                category.create_permission(&ctx.http, &category_permissions).await?;
-                let message = general_public.id.send_message(&ctx.http, |m| {
-                    m.content(format!("Click the button to play {}", ctf))
-                        .components(|c| 
-                                    c.create_action_row(|r| r.create_button(|b| b
-                                                                            .style(ButtonStyle::Primary)
-                                                                            .label("Play")
-                                                                            .custom_id(String::from("play_") + &ctf))))
-                }).await?;
-                let interaction = match message.await_component_interaction(&ctx).await {
-                    Some(x) => x,
-                    None => {
-                        msg.reply(&ctx, "Something went wrong").await?;
-                        return Ok(());
-                    },
-                };
-                let member = interaction.member.as_ref().unwrap();
-                let mut roles = member.roles.clone();
-                roles.push(role.id);
-                member.edit(&ctx.http, |m| m.roles(roles)).await?;
-                interaction.create_interaction_response(&ctx.http, |r| r.kind(InteractionResponseType::DeferredUpdateMessage)).await?;
-            }
+    if let Some(ctf) = args.single::<String>().ok() {
+        if let Some(guild_id) = msg.guild_id {
+            let category = guild_id.create_channel(&ctx.http, |c| c.name(&ctf).kind(ChannelType::Category)).await?;
+            let general_public = guild_id.create_channel(&ctx.http, |c| c.name("general-public").kind(ChannelType::Text).category(category.id)).await?;
+            let guild = guild_id.to_partial_guild(&ctx.http).await?;
+            let rgb = Colour::from_rgb(rand::random::<u8>(), rand::random::<u8>(), rand::random::<u8>());
+            let role = guild.create_role(&ctx.http, |r| r.name(&ctf).colour(rgb.0 as u64)).await?;
+            let role_permission = PermissionOverwrite {
+                allow: Permissions::all(),
+                deny: Permissions::empty(),
+                kind: PermissionOverwriteType::Role(role.id),
+            };
+            let everyone_permission = PermissionOverwrite {
+                allow: Permissions::empty(),
+                deny: Permissions::all(),
+                kind: PermissionOverwriteType::Role(RoleId::from(guild.id.0)),
+            };
+            let general = guild_id.create_channel(&ctx.http, |c| c.name("general").kind(ChannelType::Text).category(category.id)).await?;
+            general.create_permission(&ctx.http, &everyone_permission).await?;
+            general.create_permission(&ctx.http, &role_permission).await?;
+            let category_permissions = PermissionOverwrite {
+                allow: Permissions::all(),
+                deny: Permissions::empty(),
+                kind: PermissionOverwriteType::Role(role.id),
+            };
+            category.create_permission(&ctx.http, &category_permissions).await?;
+            let message = general_public.id.send_message(&ctx.http, |m| {
+                m.content(format!("Click the button to play {}", ctf))
+                    .components(|c| 
+                                c.create_action_row(|r| r.create_button(|b| b
+                                                                        .style(ButtonStyle::Primary)
+                                                                        .label("Play")
+                                                                        .custom_id(String::from("play_") + &ctf))))
+            }).await?;
+            let interaction = match message.await_component_interaction(&ctx).await {
+                Some(x) => x,
+                None => {
+                    msg.reply(&ctx, "Something went wrong").await?;
+                    return Ok(());
+                },
+            };
+            let member = interaction.member.as_ref().unwrap();
+            let mut roles = member.roles.clone();
+            roles.push(role.id);
+            member.edit(&ctx.http, |m| m.roles(roles)).await?;
+            interaction.create_interaction_response(&ctx.http, |r| r.kind(InteractionResponseType::DeferredUpdateMessage)).await?;
         }
     }
     else {
