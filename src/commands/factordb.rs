eeb25ea6f7e25622e5caea2a3714693c7b0cef5b
Andrea De Murtas <56048157+andreademurtas@users.noreply.github.com>
2023-07-10 21:02:05 +0200

    u128 factordb

--- src/commands/factordb.rs
+++ src/commands/factordb.rs
@@ -2,7 +2,7 @@ use crate::{Context, Error};
 use factordb::Number;
 
 #[poise::command(track_edits, slash_command)]
-pub async fn factordb(ctx: Context<'_>, #[description = "Number to factor"] number: u64) -> Result<(), Error> {
+pub async fn factordb(ctx: Context<'_>, #[description = "Number to factor"] number: u128) -> Result<(), Error> {
     let factors = Number::get_blocking(number).unwrap();
     ctx.send(|c| {
         c.content(format!("Factors of {}: {:?}", number, factors.factor_list()))
